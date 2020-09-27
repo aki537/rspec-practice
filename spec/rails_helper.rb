c217f939299383fe448cbf1142ea23ae9c8a3602
aki158 <hideponpon1931@gmail.com>
2020-09-27 21:31:00 +0900

    リクエストスペックを作成

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -59,4 +59,5 @@ RSpec.configure do |config|
 
   #コントローラスペックでDeviceのテストヘルパーを使用する
   config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include RequestSpecHelper, type: :request
 end
